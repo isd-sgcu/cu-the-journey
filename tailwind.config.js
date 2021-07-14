b6c5645ac1268176f1eb62550322446728dc1a52
xBraver <23140504+playerB@users.noreply.github.com>
2021-07-14 20:29:49 +0700

    🔧 Add extended color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   purge: ['src/**/*.tsx', 'public/index.html'],
   dark: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000000',
+        white: '#FFFFFF',
+        mint: '#BCE9C7',
+        green: '#9BE097',
+        yellow: '#F4AE23',
+        orange: '#F68E2E',
+        pink: '#DD7ED2',
+        purple: {
+          lightest: '#F6EFFE',
+          light: '#D0B6EF',
+          DEFAULT: '#5F229F',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
