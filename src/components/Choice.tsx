0aeb4694208c128ee125f3dce888272ee11b7f18
xBraver <skbrave1@gmail.com>
2021-07-17 03:54:35 +0700

    🎨 Improve ChoiceComponent

--- src/components/Choice.tsx
+++ src/components/Choice.tsx
@@ -6,33 +6,38 @@ const increment = (by = 1) => setCount(count() + by);
 
 const ChoiceButton = props => (
   <div>
-    <Link href={props.ref ? props.ref : "/"}>
+    <Link href={props.href}>
       <button
         onClick={() => increment(props.by)}
-        class="w-[10rem] h-[2rem] mt-[8px] rounded-full cursor-pointer
-                  text-md text-purple font-medium font-ChulaC border-[2px] border-purple
+        class="w-[12rem] h-[2rem] mt-[8px] rounded-full cursor-pointer
+                  text-md text-purple font-normal font-Mitr border-[1px] border-purple
                   hover:text-white hover:bg-purple
                   focus:outline-none focus:ring-2 focus:ring-purple focus:ring-offset-mint focus:ring-offset-1"
       >
-        {props.text} {count()}
+        {props.text} {props.href} {count()}
       </button>
     </Link>
   </div>
 );
 
-const ChoiceComponent = props => (
-  <>
-    <div class="flex h-screen justify-center items-center">
-      <div class="flex flex-col items-center min-w-[20rem]">
-        <div class="font-Mitr text-center text-xl font-medium text-purple selection:bg-purple selection:text-yellow">
-          <h1>วันนี้ของเธอเป็นวันแบบไหน</h1>
+const ChoiceComponent = props => {
+  const buttons = props.choices.map(choice => {
+    let text: string = choice[0];
+    let ref: string = choice[1] ? choice[1] : "/";
+    return <ChoiceButton by={1} text={text} href={ref} />;
+  });
+  return (
+    <>
+      <div class="flex h-screen justify-center items-center">
+        <div class="flex flex-col items-center min-w-[20rem]">
+          <div class="font-Mitr text-center text-xl font-medium text-purple selection:bg-purple selection:text-yellow">
+            <h6>{props.question}</h6>
+          </div>
+          {buttons}
         </div>
-        <ChoiceButton by={1} text="สวัสดี" ref="/1" />
-        <ChoiceButton by={2} text="ชาวไทย" ref="/2" />
-        <ChoiceButton by={3} text="เป็นอย่างไรบ้าง Test" />
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default ChoiceComponent;
