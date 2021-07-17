f347622482b36cc8c7190de16cbfab6b3af0e55c
PKhing <apimrak@gmail.com>
2021-07-17 21:46:56 +0700

    🔥 Remove unused style

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { Route } from "solid-app-router";
 import type { Component } from "solid-js";
 
 export const App: Component = () => (
-  <div class="w-screen h-screen bg-[#262626] flex justify-center items-center">
+  <div class="w-screen h-screen flex justify-center items-center">
     <div class="w-screen sm:max-w-[375px] h-screen sm:max-h-[667px] bg-white flex justify-center items-center flex-col">
       <Route />
     </div>
