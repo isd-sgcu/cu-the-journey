d71e59d88139457e59ad186df80350061ed0f411
PKhing <apimrak@gmail.com>
2021-07-17 22:53:31 +0700

    ✨ Implement pick-a-number page

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import "./firebase";
 
 export const App: Component = () => (
   <div class="w-screen h-screen flex justify-center items-center">
-    <div class="w-screen sm:max-w-[375px] h-screen sm:max-h-[667px] bg-white flex justify-center items-center flex-col">
+    <div class="w-screen sm:max-w-[375px] h-screen sm:max-h-[667px] bg-white flex justify-center items-center flex-col text-center">
       <Route />
     </div>
   </div>
