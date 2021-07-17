cc8dff2b716a48d1ee6bf7ed2dcf2e2c9da6055b
miello <partearth1212@gmail.com>
2021-07-18 00:17:01 +0700

    🐛 Fix env that not loaded into firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,17 +4,17 @@ import "firebase/storage";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.SNOWPACK_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.SNOWPACK_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.SNOWPACK_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.SNOWPACK_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.SNOWPACK_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.SNOWPACK_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.SNOWPACK_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // eslint-disable-next-line import/no-mutable-exports
-let app: firebase.app.App
+let app: firebase.app.App;
 
 if (!firebase.apps.length) {
   app = firebase.initializeApp(firebaseConfig);
