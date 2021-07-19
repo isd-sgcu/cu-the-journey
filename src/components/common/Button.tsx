d48951cc9378c01cb3a818d41f4b9e2fa30c9737
PKhing <apimrak@gmail.com>
2021-07-19 14:24:15 +0700

    🏷️ Fix button props types

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,12 +3,12 @@ import type { JSX } from "solid-js/jsx-runtime";
 
 interface IProps {
   children?: JSX.Element;
-  onClick?: () => void;
+  onClick?: (() => Promise<void>) | (() => void);
   href?: string;
 }
 
-function Button(props: IProps) {
-  const { children, onClick, href } = props;
+function Button(props: IProps & JSX.HTMLAttributes<HTMLButtonElement>) {
+  const { children, onClick, href, ...attributes } = props;
   const [, { push }] = useRouter();
 
   const handleClick = async () => {
@@ -25,6 +25,7 @@ function Button(props: IProps) {
       class="h-[40px] min-w-[150px] px-[10px] border-[1px] border-purple bg-white rounded-full m-[8px]
       hover:bg-purple-light"
       onClick={handleClick}
+      {...attributes}
     >
       {children}
     </button>
