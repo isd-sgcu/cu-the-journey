9eb96afffd6f84fe1e44950e8adf6d946faa11c2
PKhing <apimrak@gmail.com>
2021-07-19 16:04:22 +0700

    💄 Edit souvenir styles and connect to i18n

--- src/locales/th/index.ts
+++ src/locales/th/index.ts
@@ -1,9 +1,11 @@
 import i18n from "./i18n.json";
 import pickANumber from "./pickANumber.json";
+import souvenir from "./souvenir.json";
 
 const th = {
   i18n,
-  pickANumber
+  pickANumber,
+  souvenir
 };
 
 export default th;
