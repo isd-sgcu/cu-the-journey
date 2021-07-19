64166996768999bd4c3853d6da3dce9e8fd4f6c4
PKhing <apimrak@gmail.com>
2021-07-19 19:27:23 +0700

    🩹 Fix wrapper bug

--- src/app.tsx
+++ src/app.tsx
@@ -13,7 +13,7 @@ export const App: Component = () => {
 
   return (
     <div class="w-screen min-h-screen flex justify-center items-center">
-      <div class="w-screen sm:max-w-[375px] min-h-screen sm:min-h-0 sm:max-h-[667px] bg-white flex justify-center items-center flex-col text-center">
+      <div class="w-screen sm:max-w-[375px] min-h-screen sm:min-h-[667px] sm:max-h-[667px] bg-white flex justify-center items-center flex-col text-center">
         <Route />
       </div>
     </div>
