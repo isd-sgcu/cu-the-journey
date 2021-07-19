43fb399ae0f2b1b1f1bb9299c53ef5417de498ff
miello <partearth1212@gmail.com>
2021-07-19 16:46:15 +0700

    ✨ Introducing new i18n which can specific page

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,8 +1,37 @@
+/* eslint-disable no-unused-vars */
+import { useI18n } from "@amoutonbrady/solid-i18n";
+
+type ITranslation = [
+  (key: string, params?: Record<string, string>, defaultValue?: string) => string,
+  {
+    add(lang: string, table: Record<string, any>): void;
+    locale: (lang?: string) => string;
+    dict: (lang: string) => Record<string, Record<string, any>>;
+  }
+];
+
+export function useTranslation(prefix?: string): ITranslation {
+  const [t, action] = useI18n();
+
+  const newT = (key: string, params?: Record<string, string>, defaultValue?: string) => {
+    if (!prefix) {
+      return t(key, params, defaultValue);
+    }
+    return t(`${prefix}.${key}`, params, defaultValue);
+  };
+
+  return [newT, action];
+}
+
 export const dict = {
   th: {
-    hello: "สวัสดีโลก"
+    i18n: {
+      hello: "สวัสดีโลก {{ name }}"
+    }
   },
   en: {
-    hello: "Hello World"
+    i18n: {
+      hello: "Hello World {{ name }}"
+    }
   }
 };
