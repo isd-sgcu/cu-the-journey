f4440f5cfc8fe8655acf578fff9162f8cb56fbd7
PKhing <apimrak@gmail.com>
2021-07-19 15:12:55 +0700

    ✨ Connect pick a number page with i18n

--- src/pages/ending/PickANumber.tsx
+++ src/pages/ending/PickANumber.tsx
@@ -1,11 +1,13 @@
 import { Link } from "solid-app-router";
 import { Index } from "solid-js";
+import { useTranslation } from "../../config/i18n";
+import Typography from "../../components/common/Typography";
 
 function Choice({ number }: { number: number }) {
   return (
     <Link href={`/souvenir?number=${number}`} class="m-2">
       <button
-        class="w-[91px] h-[91px] flex items-center justify-center"
+        class="w-[91px] h-[91px] flex items-center justify-center bg-white hover:bg-purple-light"
         style="box-shadow: 0px 4px 8px -2px #A984D4;"
       >
         <h1>{number}</h1>
@@ -15,22 +17,28 @@ function Choice({ number }: { number: number }) {
 }
 
 function PickANumber() {
+  const [t] = useTranslation("pickANumber");
   return (
     <>
       <div class="flex-grow flex flex-col items-center justify-center ">
-        <h6>ก่อนเริ่มต้นการเดินทางครั้งใหม่เพื่อสีสัน</h6>
-        <h6>ของการเดินทางครั้งนี้</h6>
-        <h6 class="mt-3 mb-6">เธอลองเลือกมาสักหมายเลขสิ</h6>
+        <Typography variant="h6">{t("p1")}</Typography>
+        <Typography variant="h6" class="mt-3 mb-6">
+          {t("p2")}
+        </Typography>
 
         <div class="flex flex-wrap justify-center max-w-[375px]">
           <Index each={Array(6)}>{(_, idx) => <Choice number={idx + 1} />}</Index>
         </div>
 
-        <Link href="/souvenir" class="mt-20 xs:mt-5">
-          <h6 class="underline">เพื่อความตื่นเต้นขอไม่เลือกดีกว่า</h6>
+        <Link href="/souvenir" class="mt-5">
+          <Typography variant="h6" class="underline">
+            {t("p3")}
+          </Typography>
         </Link>
       </div>
-      <p class="mb-3 text-orange">*อย่าลืม Capture หน้าถัดไป เพื่อ.....</p>
+      <Typography variant="p" class="mb-3 text-orange">
+        {t("footer")}
+      </Typography>
     </>
   );
 }
