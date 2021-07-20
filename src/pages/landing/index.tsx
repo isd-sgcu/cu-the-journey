372243c04b1823027b221d5405e5d49063469f65
PKhing <apimrak@gmail.com>
2021-07-20 12:43:59 +0700

    ✨ Implement landing page

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,50 @@
+import { Link } from "solid-app-router";
+
+function index() {
+  return (
+    <div
+      class="flex-grow w-full flex flex-col justify-center text-white items-center"
+      style="background: linear-gradient(180deg, #F68E2E 0%, #DD7ED2 52.08%, #5F229F 100%);"
+    >
+      <img
+        src="images/ftj-logo.svg"
+        class="w-[79px] h-[79px] self-end mr-5 mb-[29px]"
+        alt="ftj-logo"
+      />
+
+      <h6 class="" style="text-shadow: 2px 2px #F46C2A;">
+        Freshy the Journey เปิดประตูสู่จุฬา
+      </h6>
+      <h1 class="mt-[35px]" style="text-shadow: 4px 4px #90168B;">
+        บันทึกการเดินทาง
+      </h1>
+
+      <div style="text-shadow: 4px 4px #5F229F;">
+        <h1 class="font-bold text-[144px] mt-[50px]">CU</h1>
+        <h1 class="mt-11">The</h1>
+        <h1 class="text-[48px]">Journey</h1>
+      </div>
+
+      <div class="text-[#F5A7EC] flex justify-center items-center mt-[55px] relative">
+        <div class="text-right">
+          <h6 class="text-[12px] font-normal leading-[18px]">Free Your Mind,</h6>
+          <h6 class="text-[14px] font-semibold">Find Your Ways</h6>
+        </div>
+        <div class="h-[25px] w-[2px] bg-[#DD7ED2] mx-[11px]" />
+        <div class="text-left">
+          <h6 class="text-[12px] font-normal leading-[18px]">อิสระทางความคิด</h6>
+          <h6 class="text-[14px] font-semibold">สู่ชีวิตในแบบของเรา</h6>
+        </div>
+        <div class="text-[#F4AE23] font-semibold text-[48px]">
+          <p class="absolute left-[-33px] top-[-5px]">“</p>
+          <p class="absolute right-[-28px] bottom-[-20px]">”</p>
+        </div>
+      </div>
+      <Link href="/tw" class="mt-12 mb-10">
+        <button>{"<< แตะเพื่อไปต่อ >>"}</button>
+      </Link>
+    </div>
+  );
+}
+
+export default index;
