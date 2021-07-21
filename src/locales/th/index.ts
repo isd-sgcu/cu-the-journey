d2318b83ad5e70a7315310ca77b22eb96def9150
Arm (arskiir) <69001162+arskiir@users.noreply.github.com>
2021-07-21 18:11:48 +0700

    🐛 Fixed routing config for Scene 6

--- src/locales/th/index.ts
+++ src/locales/th/index.ts
@@ -4,6 +4,7 @@ import souvenir from "./souvenir";
 import scene3 from "./scene3";
 import scene4 from "./scene4";
 import scene5 from "./scene5";
+import scene6 from "./scene6";
 import scene7 from "./scene7";
 import scene9to13 from "./scene9-13";
 import scene25to29 from "./scene25-29";
@@ -15,6 +16,7 @@ const th = {
   scene3,
   scene4,
   scene5,
+  scene6,
   scene7,
   scene9to13,
   scene25to29
--- src/pages/AllScenes.ts
+++ src/pages/AllScenes.ts
@@ -1,6 +1,7 @@
 import Scene3 from "../pages/Scene3";
 import Scene4 from "../pages/Scene4";
 import Scene5 from "../pages/Scene5";
+import Scene6 from "../pages/Scene6";
 import Scene7 from "../pages/Scene7";
 import Scene9to13 from "../pages/Scene9-13";
 import Scene25to29 from "../pages/Scene25-29";
@@ -9,6 +10,7 @@ const AllScenes = {
   Scene3,
   Scene4,
   Scene5,
+  Scene6,
   Scene7,
   Scene9to13,
   Scene25to29
