a238d4e9e4c60455c35f77393e741846274090c3
Arm (arskiir) <69001162+arskiir@users.noreply.github.com>
2021-07-22 15:56:51 +0700

    ✨ Scene 16 is done

--- src/pages/Scene14-24.tsx
+++ src/pages/Scene14-24.tsx
@@ -22,9 +22,30 @@ const Scene14S1: Component = () => {
     orderKeys: ["14-order1", "14-order2"],
     buttonTextKey: "14-buttonText",
     onTapTextKey: "14-tapText",
-    t
+    t,
   };
   return <InputBoxScene {...props} />;
 };
 
-export default { Scene14S0, Scene14S1 };
+const Scene15S0: Component = () => (
+  <>
+    <NextScene page="/16-0">
+      <TextMiddle text={t("15-0")} />
+    </NextScene>
+  </>
+);
+
+const Scene16S0: Component = () => {
+  const props = {
+    isMinimized: false,
+    nextPage: "/17-0",
+    orderKeys: "16-order",
+    placeHolderKey: "16-placeholder",
+    buttonTextKey: "16-button",
+    onTapTextKey: "16-tap",
+    t,
+  };
+  return <InputBoxScene {...props} />;
+};
+
+export default { Scene14S0, Scene14S1, Scene15S0, Scene16S0 };
