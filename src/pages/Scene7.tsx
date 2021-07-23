ea963da633c0e06c97564ecedd1b585ebb8a074e
Arm (arskiir) <69001162+arskiir@users.noreply.github.com>
2021-07-23 14:58:28 +0700

    🚧 MultiStepScene in Scene 7

--- src/pages/Scene7.tsx
+++ src/pages/Scene7.tsx
@@ -1,4 +1,6 @@
 import type { Component } from "solid-js";
+import type { MultiStepScenePropsType } from "../components/MultiStepScene";
+import MultiStepScene from "../components/MultiStepScene";
 import { NextScene } from "../components/JumpTo";
 import TextComponent from "../components/Text";
 import { sceneTranslator } from "../config/i18n";
@@ -15,13 +17,14 @@ const Scene7S0: Component = () => (
   </>
 );
 
-const Scene7S1: Component = () => (
-  <>
-    <NextScene page="/7-1-1">
-      <TextMiddle text={t("7-1")} />
-    </NextScene>
-  </>
-);
+const Scene7S1: Component = () => {
+  const props: MultiStepScenePropsType = {
+    textKeys: ["7-1-a", "7-1-b", "7-1-c"],
+    nextPage: "/7-1-1",
+    t,
+  };
+  return <MultiStepScene {...props} />;
+};
 
 const Scene7S1S1: Component = () => (
   <>
@@ -39,13 +42,14 @@ const Scene7S1S2: Component = () => (
   </>
 );
 
-const Scene7S2: Component = () => (
-  <>
-    <NextScene page="/7-2-1">
-      <TextMiddle text={t("7-2")} />
-    </NextScene>
-  </>
-);
+const Scene7S2: Component = () => {
+  const props: MultiStepScenePropsType = {
+    textKeys: ["7-2-a", "7-2-b", "7-2-c"],
+    nextPage: "/7-2-1",
+    t,
+  };
+  return <MultiStepScene {...props} />;
+};
 
 const Scene7S2S1: Component = () => (
   <>
@@ -97,7 +101,7 @@ const Scene7 = {
   Scene7S2S2,
   Scene7S3,
   Scene7S3S1,
-  Scene7S3S2
+  Scene7S3S2,
 };
 
 export default Scene7;
