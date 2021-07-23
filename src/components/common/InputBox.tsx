c0adbae82bd59df57fecd12bcb9e454d231b7393
Arm (arskiir) <69001162+arskiir@users.noreply.github.com>
2021-07-23 13:02:21 +0700

    ♻️ exportable height consts for InputBox

--- src/components/common/InputBox.tsx
+++ src/components/common/InputBox.tsx
@@ -1,5 +1,8 @@
 import { Accessor, Component, createEffect, createSignal } from "solid-js";
 
+export const INPUTBOX_HEIGHT = 233; // px
+export const SMALL_INPUTBOX_HEIGHT = 50; // px
+
 type SmallInputBoxProps = {
   signal: [get: Accessor<string>, set: (v: string | ((prev: string) => string)) => string]; // eslint-disable-line
   placeHolder: string;
@@ -12,7 +15,7 @@ type InputBoxProps = SmallInputBoxProps & { isMinimized?: boolean };
 const InputBox: Component<InputBoxProps> = props => {
   const setText = props.signal[1];
   const minimizedStyle = `border-radius: 4px; width: 200px; height: ${
-    props.height || 50
+    props.height || SMALL_INPUTBOX_HEIGHT
   }px; padding: 10px 10px;`;
   const [inlineStyle, setInlineStyle] = createSignal(props.isMinimized ? minimizedStyle : "");
   createEffect(() =>
@@ -21,7 +24,7 @@ const InputBox: Component<InputBoxProps> = props => {
     ),
   );
   const className = `placeholder-primary-300 resize-none w-[311px] h-[${
-    props.height || 233
+    props.height || INPUTBOX_HEIGHT
   }px] px-[35px] py-[30px] text-[16px] text-center border-[1px] border-purple rounded-[10px] outline-none`;
 
   return (
