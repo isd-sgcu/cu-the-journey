12ad7310f35eb1ea85b733bd954044b8e8b53ce4
miello <partearth1212@gmail.com>
2021-07-23 18:06:50 +0700

    🐛 Fix missing class style

--- src/app.tsx
+++ src/app.tsx
@@ -23,26 +23,24 @@ export const App: Component = () => {
 
   return (
     <div class="relative w-screen min-h-screen flex justify-center items-center">
-      <div class="w-screen min-h-screen flex justify-center items-center">
-        <div
-          ref={ref => {
-            screenRef = ref;
-          }}
-          class="w-screen sm:max-w-[375px] min-h-screen sm:min-h-[667px] sm:max-h-[667px] bg-white flex justify-center items-center flex-col text-center"
-        >
-          <TransitionProvider>
-            <TransitionFade order={0}>
-              <Route />
-            </TransitionFade>
-          </TransitionProvider>
-        </div>
-        <AnimationScene />
-        <div class="absolute left-0 top-0 bg-white flex flex-col z-20">
-          <button onclick={() => sceneSwitcher(["bird", "ogbg"])}>Bird</button>
-          <button onclick={() => sceneSwitcher(["ogbg"])}>remove Bird</button>
-          <button onclick={() => soundControl.play("bg")}>play sound</button>
-          <button onclick={() => soundControl.muted()}>Muted</button>
-        </div>
+      <div
+        ref={ref => {
+          screenRef = ref;
+        }}
+        class="w-screen sm:max-w-[375px] min-h-screen sm:min-h-[667px] sm:max-h-[667px] flex justify-center items-center flex-col text-center z-10"
+      >
+        <TransitionProvider>
+          <TransitionFade order={0}>
+            <Route />
+          </TransitionFade>
+        </TransitionProvider>
+      </div>
+      <AnimationScene />
+      <div class="absolute left-0 top-0 bg-white flex flex-col z-20">
+        <button onclick={() => sceneSwitcher(["bird", "ogbg"])}>Bird</button>
+        <button onclick={() => sceneSwitcher(["ogbg"])}>remove Bird</button>
+        <button onclick={() => soundControl.play("bg")}>play sound</button>
+        <button onclick={() => soundControl.muted()}>Muted</button>
       </div>
     </div>
   );
