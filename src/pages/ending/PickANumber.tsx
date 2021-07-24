8d65f9ebe21a5f0711f77ad887ea312784edcdc7
miello <partearth1212@gmail.com>
2021-07-24 13:40:35 +0700

    ⚡️ Make PickANumber page can fade out when click

--- src/pages/ending/PickANumber.tsx
+++ src/pages/ending/PickANumber.tsx
@@ -1,18 +1,18 @@
-import { Link } from "solid-app-router";
 import { Index } from "solid-js";
+import { Routing } from "../../context/TransitionContext";
 import { useTranslation } from "../../config/i18n";
 import Typography from "../../components/common/Typography";
 
 function Choice({ number }: { number: number }) {
   return (
-    <Link href={`/souvenir?number=${number}`} class="m-2">
+    <Routing href={`/souvenir?number=${number}`} class="m-2">
       <button
         class="w-[91px] h-[91px] flex items-center justify-center bg-white hover:bg-purple-light"
         style="box-shadow: 0px 4px 8px -2px #A984D4;"
       >
         <h1>{number}</h1>
       </button>
-    </Link>
+    </Routing>
   );
 }
 
@@ -30,11 +30,11 @@ function PickANumber() {
           <Index each={Array(6)}>{(_, idx) => <Choice number={idx + 1} />}</Index>
         </div>
 
-        <Link href="/souvenir" class="mt-5">
+        <Routing href="/souvenir" class="mt-5">
           <Typography variant="h6" class="underline">
             {t("p3")}
           </Typography>
-        </Link>
+        </Routing>
       </div>
       <Typography variant="p" class="mb-[28px] text-[#F46C2A]">
         {t("footer")}
