47a3f40cb18ee4e278cd2b4c226d6a58ea3f960e
miello <partearth1212@gmail.com>
2021-07-25 00:42:46 +0700

    ✨ Create FadeSignal (using with background)

--- src/context/TransitionContext.tsx
+++ src/context/TransitionContext.tsx
@@ -9,6 +9,7 @@ import {
   JSX,
   useContext,
 } from "solid-js";
+import { useFadeSignal } from "./FadeSignalContext";
 import PreventRoute from "./PreventRoute";
 import RouteMapping from "./RouteMapping";
 
@@ -50,7 +51,9 @@ export const TransitionProvider: Component = props => {
   const [nowTransition, setNowTransition] = createSignal<number>(-1);
   const [isPrevented, setPrevented] = createSignal<boolean>(true);
   const [isFadeOut, setFadeOut] = createSignal(false);
+
   const [router, { replace }] = useRouter()!;
+  const { setCurrent } = useFadeSignal()!;
 
   // Reset all state that used in this context
   const resetAnimationFrame = () => {
@@ -90,6 +93,9 @@ export const TransitionProvider: Component = props => {
       }
     } else if (!isPrevented()) {
       if (transitionNumber() === maxFrame()) {
+        const nowRoute = router.current[0].path;
+
+        setCurrent(RouteMapping[nowRoute]);
         setTransitionNumber(fadeOutNumber);
       } else if (transitionNumber() === fadeOutNumber) {
         setTransitionNumber(fadeOutFinishNumber);
@@ -153,6 +159,7 @@ export const TransitionProvider: Component = props => {
       !isAnimated() &&
       next !== router.current[0].path
     ) {
+      setCurrent(next);
       setNextScene(next);
       setTransitionNumber(fadeOutNumber);
       setFadeOut(true);
