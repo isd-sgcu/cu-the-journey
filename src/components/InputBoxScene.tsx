54c25dcb118d026da81083e471e7b136cee025ae
Arm (arskiir) <69001162+arskiir@users.noreply.github.com>
2021-07-24 22:15:25 +0700

    ✨ text inside InputBox can now be accessed outside

--- src/components/InputBoxScene.tsx
+++ src/components/InputBoxScene.tsx
@@ -1,4 +1,4 @@
-import { createSignal, createEffect, Show, Component, For } from "solid-js";
+import { createSignal, createEffect, Show, Component, For, Accessor } from "solid-js";
 import Button from "./common/Button";
 import InputBox from "./common/InputBox";
 
@@ -12,6 +12,9 @@ export { StorableKeys };
 
 export type InputBoxScenePropsType = {
   isMinimized?: boolean; // shrink down the height of the textarea
+  onButtonClicked?: () => void; // on clicking the save button
+  text?: Accessor<string>; // the caller wants to access the text and use it in the upper scope
+  setText?: (v: string | ((prev: string) => string)) => string; // eslint-disable-line
   placeHolderKey: string; // placeholder of the textarea
   nextPage: string; // ex. "/9-0"
   orderKeys: string | string[]; // key or array of keys of text lines above the textarea, ex. "8-0-order"
@@ -22,21 +25,38 @@ export type InputBoxScenePropsType = {
 };
 
 const InputBoxScene: Component<InputBoxScenePropsType> = props => {
-  const { isMinimized, placeHolderKey, orderKeys, buttonTextKey, onTapTextKey, storeKey, t } =
-    props;
+  const {
+    isMinimized,
+    placeHolderKey,
+    orderKeys,
+    buttonTextKey,
+    onTapTextKey,
+    storeKey,
+    onButtonClicked,
+    t,
+    text,
+    setText,
+  } = props;
   const placeHolder = t(placeHolderKey);
-  const [text, setText] = createSignal("");
+  let textGetter: Accessor<string>;
+  let textSetter: (v: string | ((prev: string) => string)) => string; // eslint-disable-line
+  if (text === undefined && setText === undefined) [textGetter, textSetter] = createSignal("");
+  else {
+    textGetter = text as Accessor<string>;
+    textSetter = setText as (v: string | ((prev: string) => string)) => string;
+  }
   const [isButtonShown, setIsButtonShown] = createSignal(false);
   const { cancelPrevented } = useTransitionContext();
 
   createEffect(() => {
-    setIsButtonShown(text().trim() !== "");
+    setIsButtonShown(textGetter().trim() !== "");
   });
 
   const [isGoingNextScene, setIsGoingNextScene] = createSignal(false);
 
   const proceed = () => {
-    saveMessage(storeKey, text());
+    saveMessage(storeKey, textGetter());
+    if (onButtonClicked) onButtonClicked();
     setIsButtonShown(false);
     setIsGoingNextScene(true);
     cancelPrevented();
@@ -55,7 +75,7 @@ const InputBoxScene: Component<InputBoxScenePropsType> = props => {
       <InputBox
         isGoingNextScene={isGoingNextScene}
         placeHolder={placeHolder}
-        signal={[text, setText]}
+        signal={[textGetter, textSetter]}
         isMinimized={isMinimized || false}
       />
       <Show
