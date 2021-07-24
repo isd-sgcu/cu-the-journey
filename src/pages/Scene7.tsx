1be8ea2e321dc4f7fe5b5a0ce1c51c5649e34862
Arm (arskiir) <69001162+arskiir@users.noreply.github.com>
2021-07-24 11:18:39 +0700

    ♻️ use AnimationRoute in S7

--- src/pages/Scene7.tsx
+++ src/pages/Scene7.tsx
@@ -1,8 +1,6 @@
 import type { Component } from "solid-js";
-import type { MultiStepScenePropsType } from "../components/MultiStepScene";
-import MultiStepScene from "../components/MultiStepScene";
-// import { NextScene } from "../components/JumpTo";
-import TextComponent from "../components/Text";
+import { AnimationRoute } from "../components/AnimationRoute";
+import TextComponent, { addtText } from "../components/Text";
 import { sceneTranslator } from "../config/i18n";
 
 const { TextMiddle } = TextComponent;
@@ -16,18 +14,19 @@ const Scene7S0: Component = () => (
 );
 
 const Scene7S1: Component = () => {
-  const props: MultiStepScenePropsType = {
-    textKeys: ["7-1-a", "7-1-b", "7-1-c"],
-    nextPage: "/7-1-1",
-    t,
-  };
-  return <MultiStepScene {...props} />;
+  const textKeys = ["7-1-a", "7-1-b", "7-1-c"];
+  return (
+    <AnimationRoute
+      children={textKeys.map((key, index) => (
+        <span>
+          {addtText(t(key))}
+          {index === textKeys.length ? "" : <br />}
+          <br />
+        </span>
+      ))}
+    ></AnimationRoute>
+  );
 };
-// const Scene7S1: Component = () => (
-//   <>
-//     <TextMiddle text={t("7-1")} />
-//   </>
-// );
 
 const Scene7S1S1: Component = () => (
   <>
@@ -42,18 +41,19 @@ const Scene7S1S2: Component = () => (
 );
 
 const Scene7S2: Component = () => {
-  const props: MultiStepScenePropsType = {
-    textKeys: ["7-2-a", "7-2-b", "7-2-c"],
-    nextPage: "/7-2-1",
-    t,
-  };
-  return <MultiStepScene {...props} />;
+  const textKeys = ["7-2-a", "7-2-b", "7-2-c"];
+  return (
+    <AnimationRoute
+      children={textKeys.map((key, index) => (
+        <span>
+          {addtText(t(key))}
+          {index === textKeys.length ? "" : <br />}
+          <br />
+        </span>
+      ))}
+    ></AnimationRoute>
+  );
 };
-// const Scene7S2: Component = () => (
-//   <>
-//     <TextMiddle text={t("7-2")} />
-//   </>
-// );
 
 const Scene7S2S1: Component = () => (
   <>
