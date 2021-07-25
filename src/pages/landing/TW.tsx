4ec07623aad6f92d9b84ce1a266eb4c4e21cd356
PKhing <apimrak@gmail.com>
2021-07-25 13:48:56 +0700

    💄 Fix landing page style for small mobile

--- src/pages/landing/TW.tsx
+++ src/pages/landing/TW.tsx
@@ -4,15 +4,15 @@ import { useTranslation } from "../../config/i18n";
 function tw() {
   const [t] = useTranslation("landing.tw");
   return (
-    <div class="flex flex-col flex-grow w-full justify-center items-center">
-      <h3>{t("title")}</h3>
-      <Typography class="mt-[22px]">{t("p1")}</Typography>
-      <div class="bg-white max-w-[333px] rounded-[20px] w-[95%] mt-8">
+    <>
+      <h3 class="mt-6">{t("title")}</h3>
+      <Typography class="mt-[22px] xs:text-[14px]">{t("p1")}</Typography>
+      <div class="bg-white max-w-[333px] rounded-[20px] w-[95%] mt-8 mb-6">
         <Typography class="font-bold text-[14px] leading-[17.5px] tracking-[0.5px] my-3 mx-1">
           {t("p2")}
         </Typography>
       </div>
-    </div>
+    </>
   );
 }
 
