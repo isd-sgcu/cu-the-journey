a0de7abe6c9d64e25340ed40299695edb30a70cf
miello <partearth1212@gmail.com>
2021-07-25 11:58:52 +0700

    🌐 Add template for english

---
+++ src/locales/en/scene2.ts
@@ -0,0 +1,10 @@
+export default {
+  "2-0-order": "Hi. Please introduce yourself",
+  "2-0-name": "Name",
+  "2-0-namePlaceHolder": "Fill name",
+  "2-0-id": "Student ID",
+  "2-0-idPlaceHolder": "64xxxxxxx",
+  "2-0-email": "Email",
+  "2-0-emailPlaceHolder": "Fill email",
+  "2-0-buttonText": "Let the journey begin",
+};
