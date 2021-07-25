1096877e7d54473151afe08b551914109ead87f1
miello <partearth1212@gmail.com>
2021-07-25 22:50:13 +0700

    ✏️ Fix translation typo

--- src/locales/en/scene6.ts
+++ src/locales/en/scene6.ts
@@ -2,10 +2,10 @@ export default {
   "6-0": "You open the journal.",
   "6-1": "You turn to the latest entry.",
   "6-2": "Your past self had written about your journey before entering university.",
-  "6-3-button-text": "บันทึก",
-  "6-3-placeholder": "Could you please tell a story?",
+  "6-3-button-text": "Save",
+  "6-3-placeholder": "Could you tell us about it?",
   "6-3-first-line": "How were those times?",
   "6-3-second-line": "",
-  "6-3-tap-proceed": "Tap to proceed",
+  "6-3-tap-proceed": "Tap to continue",
   "6-4": "You did great, (name). You sure went through a lot.",
 };
