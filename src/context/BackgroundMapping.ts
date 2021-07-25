80704b7fba45825ff384b1fe908fab2c9beeba79
Karn Jaroenporn <karnjj@hotmail.com>
2021-07-25 22:19:13 +0700

    :lipstick: mapping all route

--- src/context/BackgroundMapping.ts
+++ src/context/BackgroundMapping.ts
@@ -65,81 +65,96 @@ export default (): IBackgroundMapping => {
     "/6-4": ["star-light-full"],
 
     //   // Scene 7
-    //   "/7-0": "/7-1",
-    //   "/7-1": "/7-1-1",
-    //   "/7-1-1": "/7-1-2",
-    //   "/7-1-2": "/7-2",
-    //   "/7-2": "/7-2-1",
-    //   "/7-2-1": "/7-2-2",
-    //   "/7-2-2": "/7-3",
-    //   "/7-3": "/7-3-1",
-    //   "/7-3-1": "/7-3-2",
-    //   "/7-3-2": "/8-0",
+    "/7-0": ["star-light-full"],
+    "/7-1": ["star-light", "paper"],
+    "/7-1-1": ["star-light", "paper"],
+    "/7-1-2": ["star-light", "paper"],
+    "/7-2": ["star-light", "clock"],
+    "/7-2-1": ["star-light", "clock"],
+    "/7-2-2": ["star-light", "clock"],
+    "/7-3": ["star-light", "phone"],
+    "/7-3-1": ["star-light", "phone"],
+    "/7-3-2": ["star-light", "phone"],
 
     //   // Scene 8
-    //   "/8-0": "/8-1",
-    //   "/8-1": "/8-2",
-    //   "/8-2": "/9-0",
+    "/8-0": ["star-light-full"],
+    "/8-1": ["star-light-full"],
+    "/8-2": ["star-light-full"],
 
     //   // Scene 9
-    //   "/9-0": "/10-0",
+    "/9-0": ["star-light-full", "book-flip"],
 
     //   // Scene 10
-    //   "/10-0": "/11-0",
+    "/10-0": ["star-light-full"],
 
     //   // Scene 11
-    //   "/11-0": "/12-0",
+    "/11-0": ["congrats"],
 
     //   // Scene 12
-    //   "/12-0": "/13-0",
+    "/12-0": ["star-light", { name: "book-close", loop: false }],
 
     //   // Scene 13
-    //   "/13-1": "/14-0",
-    //   "/13-2": "/14-0",
-    //   "/13-3": "/14-0",
+    "/13-0": ["star-light-full"],
+    "/13-1": ["star-light-full"],
+    "/13-2": ["star-light-full"],
+    "/13-3": ["star-light-full"],
 
     //   // Scene 14
-    //   "/14-0": "/14-1",
-    //   "/14-1": "/15-0",
+    "/14-0": ["star-light", { name: "book-first", loop: false }],
+    "/14-1": ["star-light", { name: "book-first", loop: false }],
 
     //   // Scene 15
-    //   "/15-0": "/16-0",
+    "/15-0": ["star-light-full"],
 
     //   // Scene 16
-    //   "/16-0": "/17-0",
+    "/16-0": ["star-light"],
 
     //   // Scene 17
-    //   "/17-0": "/18-0",
+    "/17-0": ["star-light-full"],
 
     //   // Scene 18
-    //   "/18-0": "/18-1",
-    //   "/18-1": "/19-0",
+    "/18-0": ["star-light-full"],
+    "/18-1": ["star-light-full"],
 
     //   // Scene 19
-    //   "/19-1": "/20-0",
+    "/19-0": ["star-light-full"],
+    "/19-1": ["star-light-full"],
 
     //   // Scene 20
-    //   "/20-0": "/21-0",
+    "/20-0": ["star-light-full"],
 
     //   // Scene 21
-    //   "/21-0": "/22-0",
+    "/21-0": ["star-light-full"],
 
     //   // Scene 22
-    //   "/22-0": "/23-0",
+    "/22-0": ["star-light-full"],
 
     //   // Scene 23
-    //   "/23-0": "/24-0",
+    "/23-0": ["star-light-full", "secret"],
 
     //   // Scene 24
-    //   "/24-0": "/27-0",
+    "/24-0": ["star-light-full"],
 
     //   // Scene 27
-    //   "/27-0": "/28-0",
+    "/27-0": ["star-light-full"],
 
     //   // Scene 28
-    //   "/28-0": "/29-0",
+    "/28-0": ["star-light-full"],
 
     //   // Scene 29
+    "/29-0": ["star-light-full"],
     //   "/29-0": "/pick-a-number",
+
+    //   // pick-a-number
+    "/pick-a-number": ["star-light-full"],
+
+    // souvenir
+    "/souvenir": ["souvenir-0"],
+    "/souvenir?number=1": ["souvenir-1"],
+    "/souvenir?number=2": ["souvenir-2"],
+    "/souvenir?number=3": ["souvenir-3"],
+    "/souvenir?number=4": ["souvenir-4"],
+    "/souvenir?number=5": ["souvenir-5"],
+    "/souvenir?number=6": ["souvenir-6"],
   };
 };
