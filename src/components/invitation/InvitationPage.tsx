93e239682b24c017518ecd1a744ea2c4c52330c4
miello <partearth1212@gmail.com>
2021-07-26 01:14:55 +0700

    ✨ Complete 4-0 and preload image

--- src/components/invitation/InvitationPage.tsx
+++ src/components/invitation/InvitationPage.tsx
@@ -6,11 +6,30 @@ interface IInvitationPage {
 
 export function InvitationPage(props: IInvitationPage) {
   const [local] = splitProps(props, ["header"]);
+
   return (
-    <div class="w-full h-full">
-      <h6>{local.header}</h6>
-      <div class="relative">
-        <div class="absolute"></div>
+    <div class="w-full h-full flex items-center flex-col">
+      <h2>{local.header}</h2>
+      <div class="relative" style={{ "max-width": "250px" }}>
+        <img
+          class="w-full"
+          alt="invite-card"
+          src="images/invite-card.png"
+          style={{ "max-width": "250px", height: "auto" }}
+        />
+        <div
+          class="absolute left-5"
+          style={{
+            transform: "rotate(-9deg)",
+            width: "157px",
+            top: "7.25rem",
+          }}
+        >
+          <p style={{ "font-size": "0.6rem", "line-height": "3.2" }}>Dear (Name)</p>
+          <h6 style={{ "font-size": "0.6rem", "line-height": "1.6" }}>FREE YOUR MIND, Find</h6>
+          <h6 style={{ "font-size": "0.6rem", "line-height": "1.6" }}>YOUR WAYS</h6>
+          <p style={{ "font-size": "0.6rem", "line-height": "1.6" }}>CU at (Faculty)</p>
+        </div>
       </div>
     </div>
   );
