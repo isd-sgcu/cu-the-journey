40b2889fcebd15c1f9d37c0c8905179071469e6d
miello <partearth1212@gmail.com>
2021-07-26 11:31:44 +0700

    🐛 Fix problem when redirect to beginning

--- src/context/FadeSignalContext.tsx
+++ src/context/FadeSignalContext.tsx
@@ -1,4 +1,5 @@
-import { Accessor, createContext, createSignal, useContext } from "solid-js";
+import { useRouter } from "solid-app-router";
+import { Accessor, createContext, createEffect, createSignal, useContext } from "solid-js";
 
 interface IFadeSignalContext {
   current: Accessor<string>;
@@ -10,6 +11,19 @@ const FadeSignalContext = createContext<IFadeSignalContext>();
 export function FadeSignalProvider(props: { children: any }) {
   const { pathname, search } = window.location;
   const [current, setCurrent] = createSignal(pathname + search);
+  const [router] = useRouter()!;
+
+  createEffect(() => {
+    const nowRoute = router.current[0];
+    let mainRoute = nowRoute.path;
+
+    if (typeof router.query.number === "string") {
+      mainRoute += `?number=${router.query.number}`;
+    }
+
+    setCurrent(mainRoute);
+  });
+
   return (
     <FadeSignalContext.Provider value={{ current, setCurrent }}>
       {props.children}
