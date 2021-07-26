8c7e5a07b08a4daf87ce57a2c042921242ae4976
miello <partearth1212@gmail.com>
2021-07-26 16:13:16 +0700

    💄 Enlarge the card size

--- src/components/invitation/InvitationPage.tsx
+++ src/components/invitation/InvitationPage.tsx
@@ -11,35 +11,34 @@ export function InvitationPage(props: IInvitationPage) {
   return (
     <div class="w-full h-full flex items-center flex-col">
       <h2>{local.header}</h2>
-      <div class="relative" style={{ "max-width": "250px" }}>
+      <div class="relative" style={{ "max-width": "300px" }}>
         <img
           class="w-full"
           alt="invite-card"
           src="images/invite-card.png"
-          style={{ "max-width": "250px", height: "auto" }}
+          style={{ "max-width": "300px", height: "auto" }}
         />
         <div
-          class="absolute left-6 px-2"
+          class="absolute left-10 px-2"
           style={{
             transform: "rotate(-9deg)",
             width: "157px",
-            top: "7.25rem",
+            top: "8.25rem",
           }}
         >
           <p
             style={{
-              "font-size": "0.6rem",
+              "font-size": "0.8rem",
               "line-height": "1.6",
-              "margin-bottom": "10px",
               overflow: "hidden",
               "white-space": "nowrap",
             }}
           >
             {replaceLine("Dear (name)")}
           </p>
-          <h6 style={{ "font-size": "0.6rem", "line-height": "1.6" }}>FREE YOUR MIND, Find</h6>
-          <h6 style={{ "font-size": "0.6rem", "line-height": "1.6" }}>YOUR WAYS</h6>
-          <p style={{ "font-size": "0.6rem", "line-height": "1.6" }}>
+          <h6 style={{ "font-size": "0.8rem", "line-height": "1.6" }}>FREE YOUR MIND, Find</h6>
+          <h6 style={{ "font-size": "0.8rem", "line-height": "1.6" }}>YOUR WAYS</h6>
+          <p style={{ "font-size": "0.8rem", "line-height": "1.6" }}>
             {replaceLine("CU at (faculty)")}
           </p>
         </div>
